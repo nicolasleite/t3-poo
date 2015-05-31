34eb2157ade9ff71b02e3d77478b465682e26fe9
nicolasleite <nicolasleite96@gmail.com>
2015-05-31 17:03:21 -0300

    Small changes

--- src/libraryOOP/RegisterOfBooks.java
+++ src/libraryOOP/RegisterOfBooks.java
@@ -4,188 +4,188 @@ import java.util.*;
 import java.io.*;
    
 class Book {
-    private String name;
-    private String code;
-    private String type;
-    private String availability;
+	private String name;
+	private String code;
+	private String type;
+	private String availability;
    
-    public Book(String name, String code, String type, String availability) {
-        this.name = name;
-        this.code = code;
-        this.type = type;
-        this.availability = availability;
-    }
+	public Book(String name, String code, String type, String availability) {
+		this.name = name;
+		this.code = code;
+		this.type = type;
+		this.availability = availability;
+	}
    
-    public Book(String csv) {
-        String[] values = csv.split(",");
-        name = values[0];
-        code = values[1];
-        type = values[2];
-        availability = values[3];
-    }
+	public Book(String csv) {
+		String[] values = csv.split(",");
+		name = values[0];
+		code = values[1];
+		type = values[2];
+		availability = values[3];
+	}
    
-    public String getName(){
-        return name;
-    }
+	public String getName(){
+		return name;
+	}
    
-    public String getCode(){
-        return code;
-    }
+	public String getCode(){
+		return code;
+	}
    
-    public String getType(){
-        return type;
-    }
+	public String getType(){
+		return type;
+	}
    
-    public String getAvailability(){
-        return availability;
-    }
+	public String getAvailability(){
+		return availability;
+	}
    
-    public String toString() {
-        return "Nome: " + name + "\nCódigo: " + code + "\nTipo: " + type + "\nDisponibilidade: " + availability;
-    }
+	public String toString() {
+		return "Nome: " + name + "\nCódigo: " + code + "\nTipo: " + type + "\nDisponibilidade: " + availability;
+	}
    
-    public String toStringCSV() {
-        return name + "," + code + "," + type + "," + availability + "\n";
-    }
+	public String toStringCSV() {
+		return name + "," + code + "," + type + "," + availability + "\n";
+	}
    
-    public int loanBook(typeOfUser) {
-        if (typeOfUser == "Community" && type == "textbook"){
-            System.out.println ("This user doesn't have permission to loan this book!!");
-            return 1;
-        }
-        availability = "Unavailable";
-        return 0;
-    }
+	public int loanBook(typeOfUser) {
+		if (typeOfUser == "Community" && type == "textbook"){
+			System.out.println ("This user doesn't have permission to loan this book!!");
+			return 1;
+		}
+		availability = "Unavailable";
+		return 0;
+	}
    
-    public void returnBook() {
-        availability = "Available";
-    }
+	public void returnBook() {
+		availability = "Available";
+	}
 }
    
 public class RegisterOfBooks {
-    List<Book> books;
-    private BufferedReader in;
+	List<Book> books;
+	private BufferedReader in;
    
-    public RegisterOfBooks(String bookfile) {
-        books = new ArrayList<Book>();
-        try {
-            in = new BufferedReader (new FileReader(bookfile));
-            String csv;
-            while ((csv = in.readLine()) != null) {
-                books.add(new Book(csv));
-            }
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("File " + bookfile + " was not found!");
-        }
-        catch(IOException e) {
-            System.out.println("Error reading the file!");
-        }
-    }
+	public RegisterOfBooks(String bookfile) {
+		books = new ArrayList<Book>();
+		try {
+			in = new BufferedReader (new FileReader(bookfile));
+			String csv;
+			while ((csv = in.readLine()) != null) {
+				books.add(new Book(csv));
+			}
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("File " + bookfile + " was not found!");
+		}
+		catch(IOException e) {
+			System.out.println("Error reading the file!");
+		}
+	}
    
-    public void addNewBook(String name, String code, String type, String availability, String bookfile) throws IOException {
-         
-        //não precisa disso aqui não, tava brisando e vai dar erro  
-        //books.add(new Book(name, code, type, availability));
-           
-        BufferedWriter out = new BufferedWriter(new FileWriter(bookfile, true));
-        out.write(name + "," + code + "," + type + "," + availability);
-        out.close();
-    }
+	public void addNewBook(String name, String code, String type, String availability, String bookfile) throws IOException {
+		 
+		//não precisa disso aqui não, tava brisando e vai dar erro  
+		//books.add(new Book(name, code, type, availability));
+		   
+		BufferedWriter out = new BufferedWriter(new FileWriter(bookfile, true));
+		out.write(name + "," + code + "," + type + "," + availability + "\n");
+		out.close();
+	}
    
 /*  public void removeBook (String bookfile, String code) {
-        RegisterOfBooks rb = new RegisterOfBooks (bookfile);
-        List<Book> newList =
-            rb.books
-            .stream()
-            .filter(s -> !(s.getCode().equals(code)));
-            .collect(Collectors.toList());
+		RegisterOfBooks rb = new RegisterOfBooks (bookfile);
+		List<Book> newList =
+			rb.books
+			.stream()
+			.filter(s -> !(s.getCode().equals(code)));
+			.collect(Collectors.toList());
    
-    }*/
+	}*/
    
-    public void printEverything (String bookfile) {
-        RegisterOfBooks rb = new RegisterOfBooks (bookfile);
+	public void printEverything (String bookfile) {
+		RegisterOfBooks rb = new RegisterOfBooks (bookfile);
    
-        rb.books
-            .stream()
-            .sorted()
-            .forEach(s -> System.out.print(s.toString() + "\n"));
-    }
+		rb.books
+			.stream()
+			.sorted()
+			.forEach(s -> System.out.print(s.toString() + "\n"));
+	}
    
-    public void printAvailableOnly (String bookfile) {
-        RegisterOfBooks rb = new RegisterOfBooks (bookfile);
+	public void printAvailableOnly (String bookfile) {
+		RegisterOfBooks rb = new RegisterOfBooks (bookfile);
    
-        rb.books
-            .stream()
-            .filter(s -> s.getAvailability().equals("Available"))
-            .sorted()
-            .forEach(s -> System.out.print(s.toString() + "\n"));
-    }
+		rb.books
+			.stream()
+			.filter(s -> s.getAvailability().equals("Available"))
+			.sorted()
+			.forEach(s -> System.out.print(s.toString() + "\n"));
+	}
    
-    public void searchBookByName (String bookfile, String name){
-        RegisterOfBooks rb = new RegisterOfBooks (bookfile);
-        rb.books
-            .stream()
-            .filter(s -> s.getName().equals(name))
-            .sorted()
-            .forEach(s -> System.out.print(s.toString() + "\n"));
-    }
+	public void searchBookByName (String bookfile, String name){
+		RegisterOfBooks rb = new RegisterOfBooks (bookfile);
+		rb.books
+			.stream()
+			.filter(s -> s.getName().equals(name))
+			.sorted()
+			.forEach(s -> System.out.print(s.toString() + "\n"));
+	}
    
-    public void searchBookByCode (String bookfile, String code){
-        RegisterOfBooks rb = new RegisterOfBooks (bookfile);
-        rb.books
-            .stream()
-            .filter(s -> s.getCode().equals(code))
-            .sorted()
-            .forEach(s -> System.out.print(s.toString() + "\n"));
-    }
+	public void searchBookByCode (String bookfile, String code){
+		RegisterOfBooks rb = new RegisterOfBooks (bookfile);
+		rb.books
+			.stream()
+			.filter(s -> s.getCode().equals(code))
+			.sorted()
+			.forEach(s -> System.out.print(s.toString() + "\n"));
+	}
    
-    public int loanBook (String bookfile, String name, String typeOfUser) throws IOException {
-        RegisterOfBooks rb = new RegisterOfBooks (bookfile);
-        int i, flag;
-        Book aux;
+	public int loanBook (String bookfile, String name, String typeOfUser) throws IOException {
+		RegisterOfBooks rb = new RegisterOfBooks (bookfile);
+		int i, flag;
+		Book aux;
  
-        //false overwrite the file
-        BufferedWriter out = new BufferedWriter(new FileWriter(bookfile, false));
+		//false overwrite the file
+		BufferedWriter out = new BufferedWriter(new FileWriter(bookfile, false));
    
-        rb.books
-            .stream()
-            .filter(s -> s.getName().equals(name))
-            .findAny()
-            .filter(s -> s.getAvailability().equals("Available"))
-            .ifPresent(s -> (flag = s.loanBook(typeOfUser)));
+		rb.books
+			.stream()
+			.filter(s -> s.getName().equals(name))
+			.findAny()
+			.filter(s -> s.getAvailability().equals("Available"))
+			.ifPresent(s -> (flag = s.loanBook(typeOfUser)));
    
-        for (i=0; i<rb.books.size(); i++) {
-            aux = rb.books.get(i);
-            out.write(aux.toStringCSV());
-        }
+		for (i=0; i<rb.books.size(); i++) {
+			aux = rb.books.get(i);
+			out.write(aux.toStringCSV());
+		}
  
-        out.close();
-        return flag;
-    }
+		out.close();
+		return flag;
+	}
    
-    public void returnBook (String bookfile, String name) throws IOException {
-        RegisterOfBooks rb = new RegisterOfBooks (bookfile);
-        int i;
-        Book aux;
+	public void returnBook (String bookfile, String name) throws IOException {
+		RegisterOfBooks rb = new RegisterOfBooks (bookfile);
+		int i;
+		Book aux;
  
-        //false overwrite the file
-        BufferedWriter out = new BufferedWriter(new FileWriter(bookfile, false));
+		//false overwrite the file
+		BufferedWriter out = new BufferedWriter(new FileWriter(bookfile, false));
    
-        rb.books
-            .stream()
-            .filter(s -> s.getName().equals(name))
-            .findAny()
-            .filter(s -> s.getAvailability().equals("Unavailable"))
-            .ifPresent(s -> s.returnBook());
-         
-        for (i=0; i<rb.books.size(); i++) {
-            aux = rb.books.get(i);
-            out.write(aux.toStringCSV());
-        }
+		rb.books
+			.stream()
+			.filter(s -> s.getName().equals(name))
+			.findAny()
+			.filter(s -> s.getAvailability().equals("Unavailable"))
+			.ifPresent(s -> s.returnBook());
+		 
+		for (i=0; i<rb.books.size(); i++) {
+			aux = rb.books.get(i);
+			out.write(aux.toStringCSV());
+		}
    
-        out.close();
+		out.close();
  
-        return rb.getType();
-    }
+		return rb.getType();
+	}
 }
--- src/libraryOOP/RegisterOfLoans.java
+++ src/libraryOOP/RegisterOfLoans.java
@@ -1,112 +1,110 @@
 package libraryOOP;
  
 class Loan {
-    private String userName;
-    private String bookName;
-    private boolean isReturned;
-    private LocalDateTime dateOfLoan;
-    private LocalDateTime limitDateOfReturning;
+	private String userName;
+	private String bookName;
+	private boolean isReturned;
+	private LocalDateTime dateOfLoan;
+	private LocalDateTime limitDateOfReturning;
  
-    public Loan (String csv) {
-        String[] values = csv.split(",");
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd'/'MM'/'yy");
+	public Loan (String csv) {
+		String[] values = csv.split(",");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd'/'MM'/'yy");
  
-        userName = values[0];
-        bookName = values[1];
-        isReturned = Boolean.parseBoolean(values[2]);
-        dateOfLoan = LocalDateTime.parse(values[3], formatter);
-        limitDateOfReturning = LocalDateTime.parse(values[4], formatter);
-    }
+		userName = values[0];
+		bookName = values[1];
+		isReturned = Boolean.parseBoolean(values[2]);
+		dateOfLoan = LocalDateTime.parse(values[3], formatter);
+		limitDateOfReturning = LocalDateTime.parse(values[4], formatter);
+	}
  
-    public Loan (String userName, String bookName, boolean isReturned, LocalDateTime dateOfLoan, LocalDateTime limitDateOfReturning) {
-        this.userName = userName;
-        this.bookName = bookName;
-        this.isReturned = isReturned; 
-        this.dateOfLoan = dateOfLoan;
-        this.limitDateOfReturning = limitDateOfReturning; 
-    }
+	public Loan (String userName, String bookName, boolean isReturned, LocalDateTime dateOfLoan, LocalDateTime limitDateOfReturning) {
+		this.userName = userName;
+		this.bookName = bookName;
+		this.isReturned = isReturned; 
+		this.dateOfLoan = dateOfLoan;
+		this.limitDateOfReturning = limitDateOfReturning; 
+	}
  
-    public String getUserName () {
-        return userName;
-    }
+	public String getUserName () {
+		return userName;
+	}
  
-    public String getBookName () {
-        return bookName;
-    }
+	public String getBookName () {
+		return bookName;
+	}
  
-    public Boolean isReturned () {
-        return isReturned;
-    }
+	public Boolean isReturned () {
+		return isReturned;
+	}
  
-    public LocalDateTime getDateOfLoan () {
-        return dateOfLoan;
-    }
+	public LocalDateTime getDateOfLoan () {
+		return dateOfLoan;
+	}
  
-    public LocalDateTime getLimitDate () {
-        return limitDateOfReturning;
-    }
+	public LocalDateTime getLimitDate () {
+		return limitDateOfReturning;
+	}
  
-    public toString () {
-        return "User: " + userName + "\nBook code: " + bookName + "\nDate of loan: " + dateOfLoan + "\nLimit date of returning the book: " + limitDateOfReturning + "\n";
-    }
+	public toString () {
+		return "User: " + userName + "\nBook code: " + bookName + "\nDate of loan: " + dateOfLoan + "\nLimit date of returning the book: " + limitDateOfReturning + "\n";
+	}
  
-    public toStringCSV () {
-        return userName + "," + bookName + "," + dateOfLoan + "," + limitDateOfReturning;
-    }
+	public toStringCSV () {
+		return userName + "," + bookName + "," + dateOfLoan + "," + limitDateOfReturning;
+	}
 }
  
 public class RegisterOfLoans {
-    List<Loan> loans;
+	List<Loan> loans;
  
-    //construtor with name of file as argument
-    public RegisterOfLoans(String loanFile) {
-        loans = new ArrayList<Loan>();
-        try {
-            BufferedReader in = new BufferedReader(new FileReader(loanFile));
-            String csv;
-            while ((csv = in.readLine()) != null) {
-                loans.add(new Loan(csv));
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("File " + loanFile + " was not found!");
-        } catch (IOException e) {
-            System.out.println("Error reading the file!");
-        }
-    }
+	//construtor with name of file as argument
+	public RegisterOfLoans(String loanFile) {
+		loans = new ArrayList<Loan>();
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(loanFile));
+			String csv;
+			while ((csv = in.readLine()) != null) {
+				loans.add(new Loan(csv));
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File " + loanFile + " was not found!");
+		} catch (IOException e) {
+			System.out.println("Error reading the file!");
+		}
+	}
+
+	//não ta funcionando ainda
+	public void loanBook (String loanfile, String user, String book, LocalDateTime date, String typeOfUser) {
+		RegisterOfLoans rl = new RegisterOfLoans(loanfile);
+		int daysToReturn, i, lateLoans=0, loanedBooks=0;
+		Book aux;
  
+		for (i=0; i<rl.loans.size(); i++) {
+			aux = rl.loans.get(i);
+			if (aux.getUserName() == user && !(aux.isReturned)) {
+				loanedBooks++;
+				if (date.isAfter(aux.getLimitDate)) 
+					lateLoans++;
+			} 
+		}
+
+		switch (typeOfUser) {
+			case "Community":
+				daysToReturn = 15;
+				break;
+			case "Student":
+				daysToReturn = 30;
+				break;
+			case "Teacher":
+				daysToReturn = 60;
+				break;
+		}
  
+		LocalDateTime returnDate = LocalDateTime.plusDays(daysToReturn);
  
-    //não ta funcionando ainda
-    public void loanBook (String loanfile, String user, String book, LocalDateTime date, String typeOfUser) {
-        RegisterOfLoans rl = new RegisterOfLoans(loanfile);
-        int daysToReturn, i, lateLoans=0, loanedBooks=0;
-        Book aux;
- 
-        for (i=0; i<rl.loans.size(); i++) {
-            aux = rl.loans.get(i);
-            if (aux.getUserName() == user && !(aux.isReturned)) {
-                loanedBooks++;
-                if (date.isAfter(aux.getLimitDate)) 
-                    lateLoans++;
-            } 
-        }
- 
-        switch (typeOfUser) {
-            case "Community":
-                daysToReturn = 15;
-                break;
-            case "Student":
-                daysToReturn = 30;
-                break;
-            case "Teacher":
-                daysToReturn = 60;
-                break;
-        }
- 
-        LocalDateTime returnDate = LocalDateTime.plusDays(daysToReturn);
- 
-        BufferedWritter out = new BufferedWritter(new FileWritter(loanfile, true));
-         
-        out.write (user + "," + book + "," + date + "," + returnDate);  
-    }
+		BufferedWritter out = new BufferedWritter(new FileWritter(loanfile, true));
+			 
+		out.write (user + "," + book + "," + date + "," + returnDate);  
+	}
 }
